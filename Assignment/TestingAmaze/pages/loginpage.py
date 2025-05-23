b261f7772790e4a070df2c5cd42dd995c2f79738
William Thomas <William.Thomas@in.bosch.com>
2025-05-23 10:28:15 +0530

    Remove unwanted folders, keep only folder-to-keep

--- Assignment/TestingAmaze/pages/loginpage.py
+++
@@ -1,4 +0,0 @@
-loginid = "css=#loginusername"
-loginpwd = "css=#loginpassword"
-clicklogin = "id=login2"
-loginBtn = "css=button[onclick=\"logIn()\"]" 
--- Day-05/testscripts/Pages/CheckoutPage.py
+++
@@ -1,8 +0,0 @@
-# Define variables in Robot Framework-compatible format
-firstname = "#first-name"
-lastname = "#last-name"
-postalcode = "#postal-code"
-continuebutton = "#continue"
-cancelbtn = "#cancel"
-finishbtn = "#finish"
-checkoutComplete = "h2.complete-header"
